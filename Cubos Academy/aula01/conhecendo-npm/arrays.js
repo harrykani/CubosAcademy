e3688f2655cb5fc15d946d042f68a62c6ae7cd87
Bruno Xavier <102085342+harrykani@users.noreply.github.com>
2023-07-21 16:27:26 -0300

    Delete Cubos Academy/aula01 directory

--- Cubos Academy/aula01/conhecendo-npm/arrays.js
+++
@@ -1,8 +0,0 @@
-const arrayNumeros = [1, 2, 4, 4, 6, 7, 1];
-
-const arrayLetras = ['a', 'a', 's', 'c'];
-
-module.exports = {
-    arrayNumeros,
-    arrayLetras
-}
--- Cubos Academy/aula01/conhecendo-npm/index.js
+++
@@ -1,7 +0,0 @@
-const {uniq} = require('lodash'); // require - module.exports
-const {arrayNumeros, arrayLetras} = require('./arrays');
-const arNumUnico = uniq(arrayNumeros);
-const arLetUnico = uniq(arrayLetras);
-
-console.log(arNumUnico);
-console.log(arLetUnico);
